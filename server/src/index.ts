abba711b8bdc8f7ed9bf2ea09ca0b3efd2ff9a8b
Pacôme Françon <pacome.francon@epitech.eu>
2020-01-08 19:05:05 +0100

    feat(server): handle HTTP 404 + add bodyParser for json + some npm deps

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import compression from 'compression';
 import helmet from 'helmet';
+import bodyParser from 'body-parser';
 
 import about from './about';
 
@@ -9,8 +10,12 @@ const PORT = process.env.PORT || 8080;
 
 app.use(compression());
 app.use(helmet());
+app.use(bodyParser.json());
 
 app.use('/', about);
+app.use((req, res, next) => {
+    res.status(404).end();
+});
 
 app.listen(PORT, () => {
     console.log(`server running on ${PORT}`);
