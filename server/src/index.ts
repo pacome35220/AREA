4a1960e715b01a258072a0e444181181d606edcb
Pacôme Françon <pacome.francon@epitech.eu>
2020-01-20 17:29:35 +0100

    fix(server): enable CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import compression from 'compression';
 import helmet from 'helmet';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 
 import about from './routes/about';
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(compression());
 app.use(helmet());
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/', about);
