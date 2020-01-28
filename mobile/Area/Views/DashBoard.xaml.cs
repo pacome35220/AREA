b55eedcaae3ff8fcb48df14735ffbe1262bf1531
hadi bereksi <hadi-ilies.bereksi-reguig@epitech.eu>
2020-01-28 18:47:36 +0100

    fix(controllers/): change 201 by 200

---
+++ mobile/Area/Views/DashBoard.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Area.Views
+{
+	public partial class DashBoard : ContentPage
+	{
+		public DashBoard()
+		{
+			InitializeComponent();
+		}
+	}
+}
