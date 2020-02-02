77da5837d6afc144ab3b3aef1fac98438850ff27
hadi bereksi <hadi-ilies.bereksi-reguig@epitech.eu>
2020-02-02 23:32:12 +0100

    feat(mobile): http requests are alloed on ios now !
    ps: not sure for UWP Besides visual studio for macos is the most "buggy" software on mac that I have
    never seen

--- mobile/Area/Views/CreateAccount.xaml.cs
+++ mobile/Area/Views/CreateAccount.xaml.cs
@@ -30,14 +30,21 @@ namespace Area.Views
 			var authData = string.Format("{0}:{1}", email, password);
 			var authHeaderValue = Convert.ToBase64String(Encoding.UTF8.GetBytes(authData));
 
-			_client = new HttpClient(); //check new NSUrlSessionHandler() for ios
+			_client = new HttpClient(); ///NSUrlSessionHandler() by default for ios
 			_client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authHeaderValue);
 
 			var stringContent = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
 
 			/*connect to api here !*/
 			await DisplayAlert("Test", JsonConvert.SerializeObject(user), "OK");
-			_client.BaseAddress = new Uri("http://10.0.2.2:8080"); //set base url. android's localhost: 10.0.2.2
+
+			if (Device.RuntimePlatform == Device.iOS)
+				_client.BaseAddress = new Uri("http://localhost:8080"); //set base url. ios's localhost: 127.0.0.1
+			else if (Device.RuntimePlatform == Device.Android)
+				_client.BaseAddress = new Uri("http://10.0.2.2:8080"); //set base url. android's localhost: 10.0.2.2
+			else
+				_client.BaseAddress = new Uri("http://107.0.0.1:8080"); //set base url windows 's localhost 127.0.0.1 not sure
+
 			var response = await _client.PostAsync("/user/signup", stringContent);
 			string content = await response.Content.ReadAsStringAsync();
 			await DisplayAlert("Content", content, "OK");
--- mobile/Area/Views/LoginPage.xaml.cs
+++ mobile/Area/Views/LoginPage.xaml.cs
@@ -40,11 +40,17 @@ namespace Area.Views
 			var authData = string.Format("{0}:{1}", email, password);
 			var authHeaderValue = Convert.ToBase64String(Encoding.UTF8.GetBytes(authData));
 
-			_client = new HttpClient(); //check new NSUrlSessionHandler() for ios
+			_client = new HttpClient(); //NSUrlSessionHandler() by default for ios
 			_client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authHeaderValue);
 
 			/*connect to api here !*/
-			_client.BaseAddress = new Uri("http://10.0.2.2:8080"); //set base url
+			if (Device.RuntimePlatform == Device.iOS)
+				_client.BaseAddress = new Uri("http://localhost:8080"); //set base url. ios's localhost: 127.0.0.1
+			else if (Device.RuntimePlatform == Device.Android)
+				_client.BaseAddress = new Uri("http://10.0.2.2:8080"); //set base url. android's localhost: 10.0.2.2
+			else
+				_client.BaseAddress = new Uri("http://107.0.0.1:8080"); //set base url windows
+
 			var response = await _client.GetAsync("/user/me"); // send get Request and take endpoint in param
 			string content = await response.Content.ReadAsStringAsync();
 			if (response.StatusCode == System.Net.HttpStatusCode.OK)
