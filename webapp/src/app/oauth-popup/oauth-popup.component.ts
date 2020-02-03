d7fce1dff40e4fd20b271d9e17c1933932a3d400
Camille Police <camillepolice@icloud.com>
2020-02-03 10:32:59 +0100

    feat(home): home component

---
+++ webapp/src/app/oauth-popup/oauth-popup.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-oauth-popup',
+    templateUrl: './oauth-popup.component.html',
+    styleUrls: ['./oauth-popup.component.scss']
+})
+export class OauthPopupComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
