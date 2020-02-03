45a9163982e21f1f2fb64f2a49f8e20a8c58caf5
hadi bereksi <hadi-ilies.bereksi-reguig@epitech.eu>
2020-02-04 00:51:22 +0100

    feat(mobile): save app state
    need to fix properties

--- mobile/Area/Views/DashBoard.xaml.cs
+++ mobile/Area/Views/DashBoard.xaml.cs
@@ -11,6 +11,21 @@ namespace Area.Views
 		public DashBoard()
 		{
 			InitializeComponent();
+			if (Application.Current.Properties.ContainsKey("tab_state"))
+			{
+				CurrentPage = Children[int.Parse(Application.Current.Properties["tab_state"].ToString())];
+			}
+		}
+		protected override void OnCurrentPageChanged()
+		{
+			Device.BeginInvokeOnMainThread(async () =>
+			{
+				Application.Current.Properties["tab_state"] = Children.IndexOf(CurrentPage);
+
+				await Application.Current.SavePropertiesAsync();
+			});
+
+			base.OnCurrentPageChanged();
 		}
 	}
 }
