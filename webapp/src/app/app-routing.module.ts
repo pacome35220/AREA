c8b81f444c78355f28820d3ded3ea8551e51f5cc
Camille Police <camillepolice@icloud.com>
2020-02-04 16:33:27 +0100

    feat(home): navbar creation

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { SignupFormComponent } from './landing-screen/signup-form/signup-form.component';
 import { SigninFormComponent } from './landing-screen/signin-form/signin-form.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
     { path: '', component: SignupFormComponent },
     { path: 'signup', component: SignupFormComponent },
     { path: 'signin', component: SigninFormComponent },
+    { path: 'home', component: HomeComponent },
 
     { path: '**', redirectTo: '/' }
 ];
--- webapp/src/app/home/home.component.ts
+++ webapp/src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthServiceService } from '../services/auth-service.service';
+interface Categories {
+    label: string;
+    href: string;
+}
 
 @Component({
     selector: 'app-home',
@@ -6,7 +11,29 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    categories: Array<Categories> = [
+        { label: 'Discord', href: '/services/discord' },
+        { label: 'Epitech', href: '/services/epitech-intranet' },
+        { label: 'Imgur', href: '/services/imgur' },
+        { label: 'YouTube', href: '/services/youtube' },
+        { label: 'Google Drive', href: '/services/google' },
+        { label: 'Google Calendar', href: '/services/google' },
+        { label: 'Trello', href: '/services/trello' }
+    ];
+
+    opened: boolean;
+    status: boolean;
+    authService: AuthServiceService;
+
     constructor() {}
 
     ngOnInit() {}
+
+    toggleStatus() {
+        this.status = !this.status;
+    }
+
+    logOut() {
+        this.authService.doLogout;
+    }
 }
