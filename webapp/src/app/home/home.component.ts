dd2b61d4b2c15e69a8b6e68f350449a30efc99ed
Pacôme Françon <pacome.francon@epitech.eu>
2020-02-09 01:10:02 +0100

    feat(webapp): refacto auth-service + integrate Github

--- webapp/src/app/home/home.component.ts
+++ webapp/src/app/home/home.component.ts
@@ -1,116 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthServiceService } from '../services/auth-service.service';
+import { Component } from '@angular/core';
 
-interface Categorie {
-    label: string;
-    href: string;
-    quote: string;
-    image: string;
-    isGeneric: boolean;
-    isSpecific: boolean;
-}
+import { Service } from '../services/auth-service.service';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-    categories: Categorie[] = [
-        // {
-        //     label: 'Discord',
-        //     href: 'discord',
-        //     quote: 'Bring back the old Skype',
-        //     image: '../../assets/discord.svg',
-        //     isGeneric: false,
-        //     isSpecific: false
-        // },
-        // {
-        //     label: 'Epitech',
-        //     href: 'epitech',
-        //     quote: 'Shit here we go again',
-        //     image: '../../assets/epitech.svg',
-        //     isGeneric: false,
-        //     isSpecific: false
-        // },
-        // {
-        //     label: 'Imgur',
-        //     href: 'imgur',
-        //     quote: 'Your photo manager',
-        //     image: '../../assets/drive.svg', // TODO Switch image
-        //     isGeneric: false,
-        //     isSpecific: false
-        // },
-        // {
-        //     label: 'YouTube',
-        //     href: 'youtube',
-        //     quote: 'Your video player',
-        //     image: '../../assets/youtube.svg',
-        //     isGeneric: false,
-        //     isSpecific: false
-        // },
-        // {
-        //     label: 'Google Drive',
-        //     href: 'googe-drive',
-        //     quote: 'Your drive',
-        //     image: '../../assets/drive.svg',
-        //     isGeneric: false,
-        //     isSpecific: false
-        // },
-        // {
-        //     label: 'Google Calendar',
-        //     href: 'googe-calendar',
-        //     quote: 'Your calendar',
-        //     image: '../../assets/drive.svg', // TODO Switch image
-        //     isGeneric: false,
-        //     isSpecific: false
-        // },
-        // {
-        //     label: 'Trello',
-        //     href: 'trello',
-        //     quote: 'Your lovely tool project management',
-        //     image: '../../assets/trello.svg',
-        //     isGeneric: false,
-        //     isSpecific: false
-        // },
-        // {
-        //     label: 'Facebook',
-        //     href: 'facebook',
-        //     quote: 'Your wonderful social network',
-        //     image: '../../assets/trello.svg', // TODO Switch image
-        //     isGeneric: false,
-        //     isSpecific: false
-        // },
+export class HomeComponent {
+    services: Service[] = [
         {
-            label: 'Github',
-            href: 'github',
-            quote: 'wow',
-            image: '../../assets/trello.svg', // TODO Switch image
-            isGeneric: false,
-            isSpecific: false
+            name: 'Github',
+            authorizeUrl: 'https://github.com/login/oauth/authorize',
+            redirectUrl: 'http://localhost:4200/',
+            accessUrl: 'https://github.com/login/oauth/access_token',
+            clientId: '2166c135eda5e417e5ee',
+            scope: 'user repo'
         }
     ];
-
-    status: boolean;
-    authService: AuthServiceService;
-
-    constructor() {}
-
-    ngOnInit() {}
-
-    toggleStatus() {
-        this.status = !this.status;
-    }
-
-    isLogged() {
-        return this.authService.isAuthenticated();
-    }
-
-    logOut() {
-        this.authService.doLogout;
-    }
-
-    logIn() {
-        this.authService.login('', '', ''); // TODO parameters
-    }
 }
--- webapp/src/app/services/github/github.component.ts
+++ webapp/src/app/services/github/github.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import qs from 'qs';
+
+import { AuthServiceService, Service } from '../auth-service.service';
 
 @Component({
     selector: 'app-github',
@@ -6,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./github.component.scss']
 })
 export class GithubComponent implements OnInit {
+    @Input() reactionServices: Service[];
+
+    actionService: Service;
+
     image = '../../../assets/github.svg';
     title = 'Github';
     subtitle = 'wow wow wow wow wow wow wow wow';
@@ -19,7 +26,55 @@ export class GithubComponent implements OnInit {
 
     reactionType: 'generic' | 'specific';
 
-    constructor() {}
+    actionAccessToken: string;
+    reactionAccessToken: string | undefined;
 
-    ngOnInit() {}
+    constructor(private authService: AuthServiceService) {}
+
+    isAuthenticate() {
+        if (!this.actionAccessToken) {
+            return false;
+        }
+        if (this.reactionType === 'specific') {
+            return true;
+        } else if (
+            this.reactionType === 'generic' &&
+            !this.reactionAccessToken
+        ) {
+            return false;
+        }
+        return true;
+    }
+
+    callbackUrlParser(url: string) {
+        return url.match(/code=(.+)&state=(.+)/);
+    }
+
+    async authenticateAction() {
+        const authorizeUrl =
+            this.actionService.authorizeUrl +
+            '?' +
+            qs.stringify({
+                client_id: this.actionService.clientId,
+                redirect_uri: this.actionService.redirectUrl,
+                scope: 'user repo',
+                state: 'mdr',
+                allow_signup: 'true'
+            });
+
+        const OAuth2_Response = await this.authService.auth(
+            authorizeUrl,
+            this.callbackUrlParser
+        );
+
+        console.log(OAuth2_Response);
+    }
+
+    async registerAREA() {}
+
+    ngOnInit() {
+        this.actionService = this.reactionServices.find(
+            service => service.name === 'Github'
+        );
+    }
 }
