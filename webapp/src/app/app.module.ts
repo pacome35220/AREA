d066df4ed2d25e4f0db8d799b8245a5e079ab9e1
Pacôme Françon <pacome.francon@epitech.eu>
2020-02-19 00:29:52 +0100

    refactor(webapp): remove unused material design import

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -7,14 +7,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -46,14 +41,9 @@ import { AreaServiceComponent } from './services/area-service/area-service.compo
         BrowserAnimationsModule,
         MatIconModule,
         MatInputModule,
-        MatProgressSpinnerModule,
         MatSidenavModule,
         MatDividerModule,
-        MatListModule,
         MatButtonModule,
-        MatToolbarModule,
-        MatBadgeModule,
-        MatDialogModule,
         MatFormFieldModule,
         MatCardModule,
         MatSnackBarModule,
