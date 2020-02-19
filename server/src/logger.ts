348822f582d7c30e0e69e9204fc78d283baad757
Pacôme Françon <pacome.francon@epitech.eu>
2020-02-19 01:28:47 +0100

    improvement(server): add ip in logger message

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -3,8 +3,9 @@ import { Request, Response, NextFunction } from 'express';
 export default (req: Request, res: Response, next: NextFunction) => {
     const date = new Date().toLocaleString('en-GB', { timeZone: 'UTC' });
     const method = req.method.toUpperCase();
-    const url = req.url;
+    const url = req.originalUrl;
+    const ip = req.ip;
 
-    console.log(`${date} - ${method} ${url}`);
+    console.log(`${date} - ${ip} - ${method} ${url}`);
     next();
 };
