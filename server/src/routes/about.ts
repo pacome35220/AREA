ce37eab8a1de6eb73b7ed0f3dffe434d8386d4bd
Pacôme Françon <pacome.francon@epitech.eu>
2020-02-19 01:29:58 +0100

    feat(server): add about.json according to our API docs

--- server/src/routes/about.ts
+++ server/src/routes/about.ts
@@ -2,14 +2,68 @@ import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
+interface AboutJSON {
+    client: {
+        host: string; // client ip
+    };
+    server: {
+        current_time: number; // request timestamp
+        services: {
+            name: string; // service name
+            areas: [string, string][]; // array of area (pair of action/reaction)
+            isGenericReaction: boolean; // is this service able to receive text as generic reaction
+        }[];
+    };
+}
+
 router.get('/about.json', (req: Request, res: Response) => {
-    res.json({
-        client: req.connection.remoteAddress,
+    const about: AboutJSON = {
+        client: {
+            host: req.ip
+        },
         server: {
             current_time: Math.round(Date.now() / 1000),
-            services: []
+            services: [
+                {
+                    name: 'Github',
+                    areas: [
+                        [
+                            'If you push a new branch',
+                            'a pull request from this branch to master is create.'
+                        ]
+                    ],
+                    isGenericReaction: false
+                },
+                {
+                    name: 'Discord',
+                    areas: [
+                        [
+                            'If you are add to a channel',
+                            'a random message is send into it.'
+                        ]
+                    ],
+                    isGenericReaction: true
+                },
+                {
+                    name: 'Facebook',
+                    areas: [['TODO', 'TODO']],
+                    isGenericReaction: true
+                },
+                {
+                    name: 'Imgur',
+                    areas: [['TODO', 'TODO']],
+                    isGenericReaction: false
+                },
+                {
+                    name: 'Office365',
+                    areas: [['TODO', 'TODO']],
+                    isGenericReaction: true
+                }
+            ]
         }
-    });
+    };
+
+    res.status(200).json(about);
 });
 
 export default router;
