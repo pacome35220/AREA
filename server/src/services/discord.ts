df2e898eec9eb196dec665bd62e132061ec8a8c7
hadi bereksi <hadi-ilies.bereksi-reguig@epitech.eu>
2020-02-20 10:00:20 +0100

    fix(server): do not push code that doesn't works sob

--- server/src/services/discord.ts
+++ server/src/services/discord.ts
@@ -1,22 +1,22 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+// const Discord = require('discord.js');
+// const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+// client.on('ready', () => {
+//     console.log(`Logged in as ${client.user.tag}!`);
+// });
 
-function discordMessage(accessToken: string, message: string) {
-    client.on('ready', () => {
-        var generalChannel = client.channels.get('376373475352379411');
-        generalChannel.send('Hello, world!');
+// function discordMessage(accessToken: string, message: string) {
+//     client.on('ready', () => {
+//         var generalChannel = client.channels.get('376373475352379411');
+//         generalChannel.send('Hello, world!');
 
-        const { data } = axios.post(`https://discordapp.com/api/v6/users/@me`, {
-            headers: {
-                Authorization: `Bearer {{accessToken}}`
-            }
-        });
-        const userID = qs.parse(data).id;
+//         const { data } = axios.post(`https://discordapp.com/api/v6/users/@me`, {
+//             headers: {
+//                 Authorization: `Bearer {{accessToken}}`
+//             }
+//         });
+//         const userID = qs.parse(data).id;
 
-        client.users.get(userID).send(message);
-    });
-}
+//         client.users.get(userID).send(message);
+//     });
+// }
